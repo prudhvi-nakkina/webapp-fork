b094a03369cdf0cfc6c6e194aefe0089ba9d0e8f
prudhvi-nakkina <nakkina.p@northeastern.edu>
2023-03-02 02:16:40 -0500

    service fix 2

--- install-s.sh
+++ install-s.sh
@@ -6,15 +6,20 @@
 
     sudo yum update -y
 
+    sudo yum upgrade -y
+
     sudo yum install -y gcc-c++ make
 
     curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
     sudo yum install -y nodejs
 
-    sudo yum install unzip -y
-    cd ~/ && mkdir webapp
-    sudo mv /home/ec2-user/webapp.zip /home/ec2-user/webapp/webapp.zip
-    cd ~/webapp && unzip webapp.zip && npm i
+    sudo amazon-linux-extras install epel -y
+
+    cd /home/ec2-user && unzip ./webapp.zip
+
+    chmod -R 700 .
+
+    npm install
 
     sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
     # sudo systemctl enable webapp.service
