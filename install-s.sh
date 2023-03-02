c6467966402bbb17371f6b4dc399fc96a7bcfdbc
prudhvi-nakkina <nakkina.p@northeastern.edu>
2023-03-02 03:08:32 -0500

    packer file change

--- install-s.sh
+++ install-s.sh
@@ -2,25 +2,89 @@
 
     # Install Node.js
 
-    sleep 30
+    # sleep 30
 
-    sudo yum update -y
+    # sudo yum update -y
 
-    sudo yum upgrade -y
+    # sudo yum upgrade -y
 
-    sudo yum install -y gcc-c++ make
+    # sudo yum install -y gcc-c++ make
 
-    curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-    sudo yum install -y nodejs
+    # curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+    # sudo yum install -y nodejs
 
-    sudo amazon-linux-extras install epel -y
+    # sudo amazon-linux-extras install epel -y
 
-    cd /home/ec2-user && unzip ./webapp.zip
+    # cd /home/ec2-user && unzip ./webapp.zip
 
-    chmod -R 700 .
+    # chmod -R 700 .
 
-    npm install
+    # npm install
 
-    sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+    # sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
     # sudo systemctl enable webapp.service
     # sudo systemctl start webapp.service
+    sudo yum update
+    sudo yum upgrade
+    sudo amazon-linux-extras install -y nginx1
+    sudo amazon-linux-extras install -y epel
+    # sudo yum remove libuv -y
+    # sudo yum install libuv --disableplugin=priorities
+    sudo yum install -y curl
+    curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+    sudo yum install -y nodejs
+# sudo yum install npm
+# sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+# sudo yum install -y mysql-community-server
+# sudo systemctl start mysqld
+# sudo systemctl enable mysqld
+# passwords=$(sudo grep 'temporary password' /var/log/mysqld.log | awk {'print $13'})
+# mysql -u root -p$passwords --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Sreeja@123';"
+# mysql -u root -pSreeja@123 -e "create database cloudDB;"
+
+# sudo mysql -u root -pSreeja@123 <<EOF
+# CREATE USER 'sreeja'@'localhost' IDENTIFIED BY 'Sreeja@123';
+# GRANT ALL PRIVILEGES ON cloudDB.* TO 'sreeja'@'localhost' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;
+# EOF
+
+# echo 'export DB=cloudDB' >> ~/.bashrc
+# echo 'export USER=sreeja' >> ~/.bashrc
+# echo 'export PASSWORD=Sreeja@123' >> ~/.bashrc
+# echo 'export HOST=localhost' >> ~/.bashrc
+
+    mkdir webapp
+    mv webapp.zip webapp/
+    cd webapp
+    unzip webapp.zip
+    rm webapp.zip
+# cd webapp
+    npm install
+# mkdir uploads
+    cd ..
+    sudo chmod 755 webapp
+# touch webapp.service
+# cat <<EOF >> webapp.service
+# [Unit]
+# Description=Webapp Service
+# After=network.target
+
+# [Service]
+# Environment="DB=cloudDB"
+# Environment="USER=sreeja"
+# Environment="PASSWORD=Sreeja@123"
+# Environment="HOST=localhost"
+# Type=simple
+# User=ec2-user
+# WorkingDirectory=/home/ec2-user/webapp
+# ExecStart=/usr/bin/node server.js
+# Restart=on-failure
+
+# [Install]
+# WantedBy=multi-user.target
+# EOF
+# sudo mv webapp.service /etc/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl start webapp.service
+# sudo systemctl status webapp.service
+# sudo systemctl enable webapp.service
