09fc8b162382977683ed4fafbb4e4c673053d298
prudhvi-nakkina <nakkina.p@northeastern.edu>
2023-03-27 18:51:32 -0400

    a-7 - added cloudwatch config

--- controllers/healthcontroller.js
+++ controllers/healthcontroller.js
@@ -1,13 +1,23 @@
 const asyncHandler = require('../middleware/async');
-
+const StatsD = require('hot-shots')
+var logger = require('winston');
+const AWS = require('aws-sdk');
 // @desc    health check
 // @route   GET /healthz
 // @access  Private
 exports.checkHealth = asyncHandler((req, res, next) => {
 
+    const client = new StatsD({
+        host: 'localhost',
+        port: 8125
+    });
+
+    client.increment('healthz-requests');
+
     res.status(200).json({
         success: true,
         data: "Health Check Successful"
     });
+    logger.info('Health Check Successful')
 
 });
--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require("../util/errorResponse");
+var logger = require('../middleware/logger');
 
 const errorHandler = (err, req, res, next) => {
 
@@ -8,6 +9,7 @@ const errorHandler = (err, req, res, next) => {
     // Log to console for dev
     console.log(err.stack.red);
 
+    logger.error(error.message)
     res.status(error.statusCode || 400).json({
         success: false,
         error: error.message || 'Server Error'
