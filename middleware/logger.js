7116ee576ca3b3a311f4d378eee6e9a7e01b74d9
prudhvi-nakkina <nakkina.p@northeastern.edu>
2023-03-27 19:10:57 -0400

    testing fix

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,35 +1,38 @@
 const winston = require('winston');
 
-var options = {
-    file: {
-        level: 'info',
-        filename: '/home/ec2-user/webapp/csye6225-2.log',
-        handleExceptions: true,
-        json: true,
-        maxsize: 5242880, // 5MB
-        maxFiles: 5,
-        colorize: false,
-    },
-    console: {
-        level: 'debug',
-        handleExceptions: true,
-        json: false,
-        colorize: true,
-    },
-};
+if (process.env.NODE_ENV === 'dev') {
+    var options = {
+        file: {
+            level: 'info',
+            filename: '/home/ec2-user/webapp/csye6225-2.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, // 5MB
+            maxFiles: 5,
+            colorize: false,
+        },
+        console: {
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+        },
+    };
 
-var logger = new winston.createLogger({
-    transports: [
-        new winston.transports.File(options.file),
-        new winston.transports.Console(options.console)
-    ],
-    exitOnError: false,
-});
 
-logger.stream = {
-    write: function (message, encoding) {
-        logger.info(message);
-    },
-};
+    var logger = new winston.createLogger({
+        transports: [
+            new winston.transports.File(options.file),
+            new winston.transports.Console(options.console)
+        ],
+        exitOnError: false,
+    });
+
+    logger.stream = {
+        write: function (message, encoding) {
+            logger.info(message);
+        },
+    };
+}
 
 module.exports = logger;
